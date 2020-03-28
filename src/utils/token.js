919dfc878cea6011494ccc292786725fe0cd7791
Evgene <evisotskiy@zaraffasoft.com>
2020-03-28 12:26:33 +0200

    Update packages

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken');
-const uuid = require('uuid/v4');
+const { v4: uuidv4 } = require('uuid');
 
 function generateTokenPair(userId, expiresIn = '15m') {
-    return {
-        accessToken: jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn }),
-        refreshToken: uuid(),
-    };
+  return {
+    accessToken: jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn }),
+    refreshToken: uuidv4()
+  };
 }
 
 exports.generateTokenPair = generateTokenPair;
